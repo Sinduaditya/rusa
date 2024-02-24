feceb95cdc094aa8bfc6252136a9dea97ad8efdf
SinduAditya <nduujanadi51@gmail.com>
2024-02-25 01:32:15 +0700

    feature about

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout.jsx";
 import Home from "./pages/Home.jsx";
 import Explore from "./pages/Explore.jsx";
-import DetailExplore from "./components/explore/DetailExplore.jsx";
+import DetailExplore from "./components/Explore/DetailExplore.jsx";
 import Stories from "./pages/Stories.jsx";
 import Login from "./pages/Login.jsx";
+import DetailStories from "./components/Stories/DetailStories.jsx";
+import About from "./pages/About.jsx";
 
 
 
@@ -24,7 +26,7 @@ function App() {
                     element: <Login/>,
                 },
                 {
-                    path: "/explore",
+                    path: "/Explore",
                     element: <Explore/>,
                 },
                 {
@@ -32,7 +34,15 @@ function App() {
                     element: <Stories/>,
                 },
                 {
-                    path: "/detail-explore/:id",
+                    path: "/detail-stories/",
+                    element: <DetailStories/>,
+                },
+                {
+                    path: "/about",
+                    element: <About/>,
+                },
+                {
+                    path: "/detail-Explore/:id",
                     element: <DetailExplore />,
                 },
             ],
