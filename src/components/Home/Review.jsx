8472191ea1f3eade7ec4da7f73dee717e8c2b0f6
aplicalvin <thecalvins.sims@gmail.com>
2024-02-28 22:04:46 +0700

    Update footer

--- src/components/Home/Review.jsx
+++ src/components/Home/Review.jsx
@@ -3,7 +3,7 @@ import ReviewCard from "./ReviewCard.jsx";
 function Review() {
     return (
         <>
-            <div className="relative z-10">
+            <div className="relative z-10 border-transparent border">
                 {/* Review - Start */}
                 <div className="justify-center grid gap-4 font-poppins max-w-screen-lg mx-auto my-12 px-12 py-6  md:justify-between bg-white bg-opacity-40 rounded-3xl border-2 border-white border-opacity-50 backdrop-blur-4xl">
                     {/* Review - Bagian Atas (Tulisan dan link "Lihat Semua") */}
