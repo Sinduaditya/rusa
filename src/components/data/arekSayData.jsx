e1b2f385d89a9664123138e5cba237a22698f100
SinduAditya <nduujanadi51@gmail.com>
2024-03-02 21:59:04 +0700

    says data

---
+++ src/components/data/arekSayData.jsx
@@ -0,0 +1,26 @@
+export const arekSayData = [
+    {
+        id: 1,
+        name: "Sindu Aditya",
+        title: "unforgotable hiking",
+        description: "Meskipun cuaca buruk dan cuaca dingin, pengalaman di Kawah Ijen adalah hal yang tak terlupakan. Pendakian menuju api biru memang menantang, tetapi melihat keindahan alam yang luar biasa di puncaknya sangatlah memuaskan. Meskipun perjalanan turunnya agak sulit, tetapi melihat keajaiban alam seperti ini membuat segala usaha terbayar lunas.",
+        visited: "Desember 2023",
+        write: "6 Januari 2023"
+    },
+    {
+        id: 2,
+        name: "Adam Setiawan",
+        title: "Blue Fire",
+        description: "Meskipun pemandangannya luar biasa, saya kecewa dengan perlakuan staf kereta. Mereka mengomentari berat badan saya dan membuat saya turun beberapa kali saat melewati tanjakan. Saya berharap staf kereta dapat diberi pelatihan lebih lanjut agar tidak melakukan hal-hal yang membuat pelanggan merasa tidak nyaman",
+        visited: "Desember 2023",
+        write: "8 April 2023"
+    },
+    {
+        id: 3,
+        name: "Sebastian",
+        title: "Indah",
+        description: "Setelah menapaki jalur pendakian di tengah kabut dini hari, suasana di puncak Ijen benar-benar menyiratkan kepenatan yang tak layak diperbincangkan. Meskipun tidak dapat menyaksikan blue fire karena matahari mulai bersinar, keindahan alam Ijen tetaplah menakjubkan.",
+        visited: "November 2023",
+        write: "10 Mei 2023"
+    }
+];
